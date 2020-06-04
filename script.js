4efc418d01faad772f514167a13854b6d6ef196a
MaxRandle <31084618+MaxRandle@users.noreply.github.com>
2020-06-04 22:31:05 +1200

    tweaks

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 const getLiveData = () => {
-  // Attitude values
-  const [rollErrorDiv, rollRateDiv] = document.getElementById("roll").children;
-  const [pitchErrorDiv, pitchRateDiv] = document.getElementById("pitch").children;
-  const [yawErrorDiv, yawRateDiv] = document.getElementById("yaw").children;
+	// Attitude values
+	const [rollErrorDiv, rollRateDiv] = document.getElementById("roll").children;
+	const [pitchErrorDiv, pitchRateDiv] = document.getElementById("pitch").children;
+	const [yawErrorDiv, yawRateDiv] = document.getElementById("yaw").children;
 
-  const rollError = Number(rollErrorDiv.innerText.replace("°", ""));
-  const rollRate = Number(rollRateDiv.innerText.replace(" °/s", ""));
+	const rollError = Number(rollErrorDiv.innerText.replace("°", ""));
+	const rollRate = Number(rollRateDiv.innerText.replace(" °/s", ""));
 
-  const pitchError = Number(pitchErrorDiv.innerText.replace("°", ""));
-  const pitchRate = Number(pitchRateDiv.innerText.replace(" °/s", ""));
+	const pitchError = Number(pitchErrorDiv.innerText.replace("°", ""));
+	const pitchRate = Number(pitchRateDiv.innerText.replace(" °/s", ""));
 
-  const yawError = Number(yawErrorDiv.innerText.replace("°", ""));
-  const yawRate = Number(yawRateDiv.innerText.replace(" °/s", ""));
+	const yawError = Number(yawErrorDiv.innerText.replace("°", ""));
+	const yawRate = Number(yawRateDiv.innerText.replace(" °/s", ""));
 
-  // Translational values
-  const [xRangeDiv] = document.getElementById("x-range").children;
-  const [yRangeDiv] = document.getElementById("y-range").children;
-  const [zRangeDiv] = document.getElementById("z-range").children;
+	// Translational values
+	const [xRangeDiv] = document.getElementById("x-range").children;
+	const [yRangeDiv] = document.getElementById("y-range").children;
+	const [zRangeDiv] = document.getElementById("z-range").children;
 
-  // i know this looks stupid but the programmers who made the simulator are to blame for this absolute monstrisity
-  const zRange = Number(xRangeDiv.innerText.replace(" m", ""));
-  const xRange = Number(yRangeDiv.innerText.replace(" m", ""));
-  const yRange = Number(zRangeDiv.innerText.replace(" m", ""));
+	// i know this looks stupid but the programmers who made the simulator are to blame for this absolute monstrisity
+	const zRange = Number(xRangeDiv.innerText.replace(" m", ""));
+	const xRange = Number(yRangeDiv.innerText.replace(" m", ""));
+	const yRange = Number(zRangeDiv.innerText.replace(" m", ""));
 
-  return {
-    rollError,
-    rollRate,
-    pitchError,
-    pitchRate,
-    yawError,
-    yawRate,
-    xRange,
-    yRange,
-    zRange,
-  };
+	return {
+		rollError,
+		rollRate,
+		pitchError,
+		pitchRate,
+		yawError,
+		yawRate,
+		xRange,
+		yRange,
+		zRange
+	};
 };
 
 // Controls
@@ -54,65 +54,68 @@ const translateForwardButton = document.getElementById("translate-forward-button
 const translateBackButton = document.getElementById("translate-backward-button");
 
 function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const loop = async () => {
-  while (true) {
-    const sleepTime = 100;
-    const { rollError, rollRate, pitchError, pitchRate, yawError, yawRate, xRange, yRange, zRange } = getLiveData();
+	while (true) {
+		const sleepTime = 100;
+		const { rollError, rollRate, pitchError, pitchRate, yawError, yawRate, xRange, yRange, zRange } = getLiveData();
 
-    // Attitude adjustments
-    const targetRollRate = rollError / 10;
-    const targetPitchRate = pitchError / 10;
-    const targetYawRate = yawError / 10;
+		// Attitude adjustments
+		const targetRollRate = rollError / 10;
+		const targetPitchRate = pitchError / 10;
+		const targetYawRate = yawError / 10;
 
-    // roll
-    if (rollRate > targetRollRate) {
-      rollLeftButton.click();
-    } else if (rollRate < targetRollRate) {
-      rollRightButton.click();
-    }
-    // pitch
-    if (pitchRate > targetPitchRate) {
-      pitchUpButton.click();
-    } else if (pitchRate < targetPitchRate) {
-      pitchDownButton.click();
-    }
-    // yaw
-    if (yawRate > targetYawRate) {
-      yawLeftButton.click();
-    } else if (yawRate < targetYawRate) {
-      yawRightButton.click();
-    }
+		// roll
+		if (rollRate > targetRollRate) {
+			rollLeftButton.click();
+		} else if (rollRate < targetRollRate) {
+			rollRightButton.click();
+		}
+		// pitch
+		if (pitchRate > targetPitchRate) {
+			pitchUpButton.click();
+		} else if (pitchRate < targetPitchRate) {
+			pitchDownButton.click();
+		}
+		// yaw
+		if (yawRate > targetYawRate) {
+			yawLeftButton.click();
+		} else if (yawRate < targetYawRate) {
+			yawRightButton.click();
+		}
 
-    // Translational adjustments
-    const targetXVelocity = (xRange / 1e3) * -1;
-    const targetYVelocity = (yRange / 1e3) * -1;
-    const targetZVelocity = (zRange / 1e3) * -1;
-    const { x: xVelocity, y: yVelocity, z: zVelocity } = motionVector;
+		// Translational adjustments
+		const targetXVelocity = (xRange / 500) * -1;
+		const targetYVelocity = (yRange / 500) * -1;
+		const targetZVelocity = (zRange / 1000) * -1 - 0.005;
+		const { x: xVelocity, y: yVelocity, z: zVelocity } = motionVector;
 
-    // x
-    if (xVelocity > targetXVelocity) {
-      translateLeftButton.click();
-    } else if (xVelocity < targetXVelocity) {
-      translateRightButton.click();
-    }
-    // y
-    if (yVelocity > targetYVelocity) {
-      translateDownButton.click();
-    } else if (yVelocity < targetYVelocity) {
-      translateUpButton.click();
-    }
-    // z
-    if (zVelocity > targetZVelocity) {
-      translateForwardButton.click();
-    } else if (zVelocity < targetZVelocity) {
-      translateBackButton.click();
-    }
+		const rangeTolerance = 0.05;
+		const velocityTolerance = 0.0005;
 
-    await sleep(sleepTime);
-  }
+		// x
+		if (Math.abs(xRange) > rangeTolerance && xVelocity - targetXVelocity > velocityTolerance) {
+			translateLeftButton.click();
+		} else if (Math.abs(xRange) > rangeTolerance && xVelocity < targetXVelocity) {
+			translateRightButton.click();
+		}
+		// y
+		if (Math.abs(yRange) > rangeTolerance && yVelocity - targetYVelocity > velocityTolerance) {
+			translateDownButton.click();
+		} else if (Math.abs(yRange) > rangeTolerance && yVelocity < targetYVelocity) {
+			translateUpButton.click();
+		}
+		// z
+		if (Math.abs(zRange) > rangeTolerance && zVelocity - targetZVelocity > velocityTolerance) {
+			translateForwardButton.click();
+		} else if (Math.abs(zRange) > rangeTolerance && zVelocity < targetZVelocity) {
+			translateBackButton.click();
+		}
+
+		await sleep(sleepTime);
+	}
 };
 
 loop();
