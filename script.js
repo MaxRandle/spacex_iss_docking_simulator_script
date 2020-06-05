c6043825d6f22a6f6c06702d7d8bd5387793cee5
MaxRandle <31084618+MaxRandle@users.noreply.github.com>
2020-06-06 00:45:47 +1200

    translational damping

--- script.js
+++ script.js
@@ -1,92 +1,93 @@
 function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const rotationIncrement = rotationPulseSize * toRAD * 0.01;
+// time between each loop
+const sleepTime = 100;
+
+// rotation spring constant
+const rk = 0.02;
+// rotation damping constant
+const rd = 1.8;
+
+// translation spring constant
+const tk = 0.1;
+//translation damping constant
+const td = 0;
+
 const loop = async () => {
-  const rotationIncrement = rotationPulseSize * toRAD * 0.01;
-  const translationSize = 0.001;
+	console.log("autopilot engaged");
 
-  while (true) {
-    // time between each loop
-    const sleepTime = 100;
-    // spring constant
-    const k = 0.01;
-    // damping constant
-    const d = 0.1;
+	while (true) {
+		// Rotaion adjustments
+		const rotationX = camera.rotation.x;
+		const rotationY = camera.rotation.y;
+		const rotationZ = camera.rotation.z;
+		// roll right decreaces
+		const targetRotationXRate = -rk * rotationX - rd * currentRotationX;
+		const targetRotationYRate = -rk * rotationY - rd * currentRotationY;
+		const targetRotationZRate = -rk * rotationZ - rd * currentRotationZ;
 
-    // Rotaion adjustments
-    const rotationX = camera.rotation.x;
-    const rotationY = camera.rotation.y;
-    const rotationZ = camera.rotation.z;
-    // roll right decreaces
-    const targetRotationXRate = -k * rotationX - d * currentRotationX;
-    const targetRotationYRate = -k * rotationY - d * currentRotationY;
-    const targetRotationZRate = -k * rotationZ - d * currentRotationZ;
+		// pitch
+		if (currentRotationX > targetRotationXRate && currentRotationX - targetRotationXRate > rotationIncrement / 2) {
+			pitchDown();
+		} else if (currentRotationX < targetRotationXRate && currentRotationX - targetRotationXRate < rotationIncrement / 2) {
+			pitchUp();
+		}
 
-    // // pitch
-    // if (currentRotationX > targetRotationXRate) {
-    //   pitchDown();
-    // } else if (currentRotationX < targetRotationXRate) {
-    //   pitchUp();
-    // }
+		// yaw
+		if (currentRotationY > targetRotationYRate && currentRotationY - targetRotationYRate > rotationIncrement / 2) {
+			yawRight();
+		} else if (currentRotationY < targetRotationYRate && currentRotationY - targetRotationYRate < rotationIncrement / 2) {
+			yawLeft();
+		}
 
-    // // yaw
-    // if (currentRotationY > targetRotationYRate) {
-    //   yawRight();
-    // } else if (currentRotationY < targetRotationYRate) {
-    //   yawLeft();
-    // }
+		// roll
+		if (currentRotationZ > targetRotationZRate && currentRotationZ - targetRotationZRate > rotationIncrement / 2) {
+			rollRight();
+		} else if (currentRotationZ < targetRotationZRate && currentRotationZ - targetRotationZRate < rotationIncrement / 2) {
+			rollLeft();
+		}
 
-    // roll
-    const shouldRollRight =
-      currentRotationZ > targetRotationZRate &&
-      currentRotationZ - rotationIncrement - targetRotationZRate < currentRotationZ - targetRotationZRate;
-    const shouldRollLeft =
-      currentRotationZ < targetRotationZRate &&
-      currentRotationZ + rotationIncrement - targetRotationZRate > currentRotationZ - targetRotationZRate;
+		// // Translational adjustments
+		const xRange = camera.position.x - issObject.position.x;
+		const yRange = camera.position.y - issObject.position.y;
+		const zRange = camera.position.z - issObject.position.z;
+		const { x: currentXVelocity, y: currentYVelocity, z: currentZVelocity } = motionVector;
 
-    // if (currentRotationZ > targetRotationZRate) {
-    if (shouldRollRight) {
-      rollRight();
-    } else if (shouldRollLeft) {
-      rollLeft();
-    }
+		const targetXVelocity = -tk * xRange - td * currentXVelocity;
+		const targetYVelocity = -tk * yRange - td * currentYVelocity;
+		const targetZVelocity = -tk * zRange - td * currentZVelocity;
 
-    // // Translational adjustments
-    // const targetXVelocity = (xRange / 500) * -1;
-    // const targetYVelocity = (yRange / 500) * -1;
-    // const targetZVelocity = (zRange / 1000) * -1 - 0.005;
-    // const { x: xVelocity, y: yVelocity, z: zVelocity } = motionVector;
+		// x
+		if (currentXVelocity > targetXVelocity && currentXVelocity - targetXVelocity > translationPulseSize / 2) {
+			translateLeft();
+		} else if (currentXVelocity < targetXVelocity && currentXVelocity - targetXVelocity < translationPulseSize / 2) {
+			translateRight();
+		}
 
-    // const fineTranslationAdjustmentMagnitude = 0.001;
-    // const courseTranslationAdjustmentMagnitude = 0.005;
-    // const fineAttitudeAdjustmentMagnitude = 0.1;
-    // // const courseAttitudeAdjustmentMagnitude = ???
+		// // x
+		// if (Math.abs(xRange) > rangeTolerance && xVelocity - targetXVelocity > velocityTolerance) {
+		//   translateLeft();
+		// } else if (Math.abs(xRange) > rangeTolerance && xVelocity < targetXVelocity) {
+		//   translateRight();
+		// }
+		// // y
+		// if (Math.abs(yRange) > rangeTolerance && yVelocity - targetYVelocity > velocityTolerance) {
+		//   translateDown();
+		// } else if (Math.abs(yRange) > rangeTolerance && yVelocity < targetYVelocity) {
+		//   translateUp();
+		// }
+		// // z
+		// if (Math.abs(zRange) > rangeTolerance && zVelocity - targetZVelocity > velocityTolerance) {
+		//   translateForward();
+		// } else if (Math.abs(zRange) > rangeTolerance && zVelocity < targetZVelocity) {
+		//   translateBackward();
+		// }
 
-    // const rangeTolerance = 0.05;
-    // const velocityTolerance = 0.0005;
-
-    // // x
-    // if (Math.abs(xRange) > rangeTolerance && xVelocity - targetXVelocity > velocityTolerance) {
-    //   translateLeft();
-    // } else if (Math.abs(xRange) > rangeTolerance && xVelocity < targetXVelocity) {
-    //   translateRight();
-    // }
-    // // y
-    // if (Math.abs(yRange) > rangeTolerance && yVelocity - targetYVelocity > velocityTolerance) {
-    //   translateDown();
-    // } else if (Math.abs(yRange) > rangeTolerance && yVelocity < targetYVelocity) {
-    //   translateUp();
-    // }
-    // // z
-    // if (Math.abs(zRange) > rangeTolerance && zVelocity - targetZVelocity > velocityTolerance) {
-    //   translateForward();
-    // } else if (Math.abs(zRange) > rangeTolerance && zVelocity < targetZVelocity) {
-    //   translateBackward();
-    // }
-
-    await sleep(sleepTime);
-  }
+		await sleep(sleepTime);
+	}
 };
 
 loop();
