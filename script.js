a1e41094741c314c68792ffd1fc22a4126f57fd1
max <maxrandle@hotmail.com>
2020-10-08 21:10:31 +1300

    added autopilot button

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function htmlToElement(html) {
+  var template = document.createElement("template");
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+// add autopilot button
+let autopilotEngaged = false;
+const optionsElement = document.getElementById("options");
+const autopilotButton = htmlToElement(
+  '<div id="autopilot" title="Engage Autopilot" class="icon">A</div>'
+);
+autopilotButton.addEventListener("click", () => {
+  autopilotEngaged = !autopilotEngaged;
+});
+optionsElement.appendChild(autopilotButton);
+
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -5,7 +23,30 @@ function sleep(ms) {
 const shouldMove = (targetVelocity, currentVelocity, pulse) => {
   // will moving get me closer to my target velocity
   const newVelocity = currentVelocity + pulse;
-  return Math.abs(targetVelocity - newVelocity) < Math.abs(targetVelocity - currentVelocity);
+  return (
+    Math.abs(targetVelocity - newVelocity) <
+    Math.abs(targetVelocity - currentVelocity)
+  );
+};
+
+const shouldTogglePrecision = (distance) => {
+  const precisionThreshold = 5;
+  const precisionStatusElement = document.getElementById(
+    "precision-translation-status"
+  );
+  const precisionToggleButton = document.getElementById("toggle-translation");
+
+  if (
+    distance > precisionThreshold &&
+    precisionStatusElement.className !== "noselect large"
+  ) {
+    precisionToggleButton.click();
+  } else if (
+    distance <= precisionThreshold &&
+    precisionStatusElement.className === "noselect large"
+  ) {
+    precisionToggleButton.click();
+  }
 };
 
 const rotationIncrement = rotationPulseSize * toRAD * 0.01;
@@ -26,64 +67,87 @@ const loop = async () => {
   console.log("autopilot engaged");
 
   while (true) {
-    // rotaion
-    const rotationX = camera.rotation.x;
-    const rotationY = camera.rotation.y;
-    const rotationZ = camera.rotation.z;
-    const targetRotationXRate = -rk * rotationX - rb * currentRotationX;
-    const targetRotationYRate = -rk * rotationY - rb * currentRotationY;
-    const targetRotationZRate = -rk * rotationZ - rb * currentRotationZ;
+    if (autopilotEngaged) {
+      // should toggle translation precision
 
-    // translation
-    const xRange = camera.position.x - issObject.position.x;
-    const yRange = camera.position.y - issObject.position.y;
-    const zRange = camera.position.z - issObject.position.z;
-    const currentXVelocity = motionVector.x * 50;
-    const currentYVelocity = motionVector.y * 50;
-    const currentZVelocity = motionVector.z * 50;
-    const targetXVelocity = -tk * xRange - tb * currentXVelocity;
-    const targetYVelocity = -tk * yRange - tb * currentYVelocity;
-    const targetZVelocity = -tk * zRange - tb * currentZVelocity - 0.01;
+      // calculate rotaion velocities
+      const rotationX = camera.rotation.x;
+      const rotationY = camera.rotation.y;
+      const rotationZ = camera.rotation.z;
+      const targetRotationXRate = -rk * rotationX - rb * currentRotationX;
+      const targetRotationYRate = -rk * rotationY - rb * currentRotationY;
+      const targetRotationZRate = -rk * rotationZ - rb * currentRotationZ;
 
-    // rotation
-    if (shouldMove(targetRotationXRate, currentRotationX, -rotationIncrement)) {
-      pitchDown();
-    }
-    if (shouldMove(targetRotationXRate, currentRotationX, rotationIncrement)) {
-      pitchUp();
-    }
-    if (shouldMove(targetRotationYRate, currentRotationY, -rotationIncrement)) {
-      yawRight();
-    }
-    if (shouldMove(targetRotationYRate, currentRotationY, rotationIncrement)) {
-      yawLeft();
-    }
-    if (shouldMove(targetRotationZRate, currentRotationZ, -rotationIncrement)) {
-      rollRight();
-    }
-    if (shouldMove(targetRotationZRate, currentRotationZ, rotationIncrement)) {
-      rollLeft();
-    }
-    // translation
-    if (shouldMove(targetXVelocity, currentXVelocity, -translationPulseSize)) {
-      translateLeft();
-    }
-    if (shouldMove(targetXVelocity, currentXVelocity, translationPulseSize)) {
-      translateRight();
-    }
-    if (shouldMove(targetYVelocity, currentYVelocity, -translationPulseSize)) {
-      translateDown();
-    }
-    if (shouldMove(targetYVelocity, currentYVelocity, translationPulseSize)) {
-      translateUp();
-    }
-    if (shouldMove(targetZVelocity, currentZVelocity, -translationPulseSize)) {
-      translateForward();
-    }
-    if (shouldMove(targetZVelocity, currentZVelocity, translationPulseSize)) {
-      translateBackward();
-    }
+      // calculate translation velocities
+      const xRange = camera.position.x - issObject.position.x;
+      const yRange = camera.position.y - issObject.position.y;
+      const zRange = camera.position.z - issObject.position.z;
+      const currentXVelocity = motionVector.x * 50;
+      const currentYVelocity = motionVector.y * 50;
+      const currentZVelocity = motionVector.z * 50;
+      const targetXVelocity = -tk * xRange - tb * currentXVelocity;
+      const targetYVelocity = -tk * yRange - tb * currentYVelocity;
+      const targetZVelocity = -tk * zRange - tb * currentZVelocity - 0.01;
 
+      shouldTogglePrecision(zRange);
+
+      // rotation actuators
+      if (
+        shouldMove(targetRotationXRate, currentRotationX, -rotationIncrement)
+      ) {
+        pitchDown();
+      }
+      if (
+        shouldMove(targetRotationXRate, currentRotationX, rotationIncrement)
+      ) {
+        pitchUp();
+      }
+      if (
+        shouldMove(targetRotationYRate, currentRotationY, -rotationIncrement)
+      ) {
+        yawRight();
+      }
+      if (
+        shouldMove(targetRotationYRate, currentRotationY, rotationIncrement)
+      ) {
+        yawLeft();
+      }
+      if (
+        shouldMove(targetRotationZRate, currentRotationZ, -rotationIncrement)
+      ) {
+        rollRight();
+      }
+      if (
+        shouldMove(targetRotationZRate, currentRotationZ, rotationIncrement)
+      ) {
+        rollLeft();
+      }
+      // translation actuators
+      if (
+        shouldMove(targetXVelocity, currentXVelocity, -translationPulseSize)
+      ) {
+        translateLeft();
+      }
+      if (shouldMove(targetXVelocity, currentXVelocity, translationPulseSize)) {
+        translateRight();
+      }
+      if (
+        shouldMove(targetYVelocity, currentYVelocity, -translationPulseSize)
+      ) {
+        translateDown();
+      }
+      if (shouldMove(targetYVelocity, currentYVelocity, translationPulseSize)) {
+        translateUp();
+      }
+      if (
+        shouldMove(targetZVelocity, currentZVelocity, -translationPulseSize)
+      ) {
+        translateForward();
+      }
+      if (shouldMove(targetZVelocity, currentZVelocity, translationPulseSize)) {
+        translateBackward();
+      }
+    }
     await sleep(sleepTime);
   }
 };
